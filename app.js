99391b80c3b21a4acdab201bc5a8422bbf762c35
Mairajk <mairajkhan597@gmail.com>
2022-06-12 20:30:34 +0500

    doing............

--- app.js
+++ app.js
@@ -2,77 +2,69 @@ function start()  {
 
     let matGen1 = document.querySelector("#matGen1").value ;
     let matGen2 = document.querySelector("#matGen2").value ;
+   
     let mat1 = document.querySelector("#mat1");
     let mat2 = document.querySelector("#mat2");
+   
+    let result = document.querySelector(`#result`) ;
+ 
+        mat1.innerHTML = "";
+        mat2.innerHTML = ""; 
+        result.innerHTML = "" ;
+       
 
     for (let i = 0; i < matGen1; i++) {
         for (let j = 0; j < matGen2; j++) {
        
-            mat1.innerHTML += `<input size="1"  placeholder="0"  id = fMat1${i}_${j} class="1iB"> ` ;
+            mat1.innerHTML += `<input placeholder="0"  id="fMat1${i}_${j}"
+             class="1iB" required type="number" min="0" max="100"> ` ;
             console.log(i+1)
-        }
-        mat1.innerHTML += "<br>";
-    }
-
-    for (let i = 0; i < matGen1; i++) {
-        for (let j = 0; j < matGen2; j++) {
            
-            mat2.innerHTML += `<input size="1"  placeholder="0"  id = fMat2${i}_${j} class="2iB"> ` ;
-            console.log(i+1)
-        }
-        mat2.innerHTML += "<br>";
+            mat2.innerHTML += `<input placeholder="0"  id="fMat2${i}_${j}"
+             class="2iB" required type="number" min="0" max="100"> ` ;
+                    console.log(i+1)
+           
+            result.innerHTML +=  `<input type="number" disabled required min="0"
+             max="100"  id="result${i}_${j}"  value="0" >`;        
+        
+       }
+        mat1.innerHTML += "<br>";
+        mat2.innerHTML += "<br>"; 
+        result.innerHTML += "<br>" ;
+       
     }
 
     let checksign = document.querySelector("#checksign").value ;
     let dropDown = document.querySelector("#dropDown") ;
     dropDown.innerHTML = checksign ; 
-   
-    // for (let i = 0; i < matGen1 ; i++) {
-    //     for (let j = 0; j < matGen2; j++) {
-    //        fMat
-    //         result = 
-            
-    //     }
-        
-    // }
+    document.querySelector("#equal").innerHTML = "=" ;    
+  
 
 }
 
 
 function cal() {
     
-
-
-    let checksign = document.querySelector("#checksign").value ;
-    let dropDown = document.querySelector("#dropDown") ;
-    dropDown.innerHTML = checksign ; 
- 
-    let matGen1 = document.querySelector("#matGen1").value ;
-    let matGen2 = document.querySelector("#matGen2").value ;
-
-    let fMat1 = document.querySelector(`#fmat1${i}_${j}`).value  ;
-    let fMat2 = document.querySelector(`#fMat2${i}_${j}`).value ;
-      
-    let result = Number(fMat1) + Number(fMat2);
-
-    let gResult = document.querySelector(`#result`) ;
- 
     for (let i = 0; i < matGen1 ; i++) {
         
         for (let j = 0; j < matGen2; j++) {
-      
-        
-        gResult.innerHTML += ` <h1> ${result} </h1> ` ;   
-        
-        console.log(gResult); 
-        
+
+            document.querySelector(`#result${i}_${j}`).value =
+            +document.querySelector(`#fMat1${i}_${j}`).value +
+            +document.querySelector(`#fMat2${i}_${j}`).value ;  
         }
         
-        gResult.innerHTML += `<br>`
     }
 
-
 }
+
+
+
+
+
+
+
+//<------------Wastage--------------->
 
 
 
@@ -108,3 +100,35 @@ function cal() {
     //     target.innerHTML += "<br>";
     // }
 
+
+
+
+
+
+
+
+    
+    // for (let i = 0; i < matGen1; i++) {
+    //     for (let j = 0; j < matGen2; j++) {
+           
+    //         mat2.innerHTML += `<input size="1"  placeholder="0"  id = fMat2${i}_${j} class="2iB"> ` ;
+    //         console.log(i+1)
+    //     }
+    //     mat2.innerHTML += "<br>";
+    // }
+
+
+
+
+
+
+
+     
+    // for (let i = 0; i < matGen1 ; i++) {
+    //     for (let j = 0; j < matGen2; j++) {
+    //        fMat
+    //         result = 
+            
+    //     }
+        
+    // }
